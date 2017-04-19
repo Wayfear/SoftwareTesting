a3dea107985f7d7c6c4e8e4e63d81d66dee6b66f
DESKTOP-71C48PT\kanxuan <kanxuan1996@gmail.com>
2017-04-19 23:31:04 +0800

    初步完成

---
+++ SoftwareTesting/Pages/Experiments/Sales/SelectFile.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SoftwareTesting.Pages.Experiments.Sales
+{
+    /// <summary>
+    /// Interaction logic for BasicPage3.xaml
+    /// </summary>
+    public partial class BasicPage3 : UserControl
+    {
+        public BasicPage3()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.OpenFileDialog openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox.Text=openFileDialog1.FileName;
+            }
+        }
+
+        private void File_Parse(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
